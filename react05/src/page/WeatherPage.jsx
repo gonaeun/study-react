380f08401c8d3991ee6cb2a264f051321a3e797c
gonaeun <gonaeunn@gmail.com>
2024-08-26 11:09:42 +0900

    날씨실습

--- react05/src/page/WeatherPage.jsx
+++ react05/src/page/WeatherPage.jsx
@@ -8,6 +8,7 @@ const WeatherPage = () => {
   const [weather, setWeather] = useState(null)
   // 우리가 보고있는 정보의 형태가 객체니까 {}
   // html에서 물음표 문법 쓸거면 null로 해줘야 오류안남
+  // (null로 적어줘야 물음표 문법이 판단할 때 처음에는 값이 비어서 화면이 보여지지 않다가, 데이터 들어오면 rerendering되면서 보여짐)
 
     const getCurrentWeather = async ()=>{
         let cityName = 'Gwangju';
@@ -34,9 +35,9 @@ const WeatherPage = () => {
       <h1>WeatherPage</h1>
       <div>
         <p>도시 : {weather?.name}</p>
-        <p>온도: {weather?.main?.temp}</p>
+        <p>온도: {weather?.main.temp}</p>
         <p>날씨상태: {weather?.weather[0].description}</p>
-        <p><img src={`https://openweathermap.org/img/wn/04d@2x.png`} alt="날씨이미지"></img></p>
+        <p><img src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`} alt="날씨이미지"></img></p>
         {/* 로칼서버가 아니라 외부서버에 데이터 요청하는거라서 통신 속도에 따라 에러가 뜰 수도 있음
           >> REACT에서 물음표 문법>> 데이터가 있냐 없냐 >> 있으면 보여주고 없어도 에러 안 뜨도록 */}
       </div>
