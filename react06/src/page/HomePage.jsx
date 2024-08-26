13f0eee61470ccf1f9ccd2981b9ba3ac48fa4be9
gonaeun <gonaeunn@gmail.com>
2024-08-26 12:38:21 +0900

    유즈네이게이트로 이동하는 방법

--- react06/src/page/HomePage.jsx
+++ react06/src/page/HomePage.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
+
+    const navigate = useNavigate();
+
+    const goToProduct = ()=>{
+        navigate('/product')
+    }
   return (
     <div>
         <h1>HomePage</h1>
         <Link to={"/about"}>About 페이지로 이동</Link>
+        <br></br>
+        <button onClick={goToProduct}>ProductPage fh dlehd</button>
     </div>
   )
 }
