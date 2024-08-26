fca46bff3934ebf0453dfeb54a89b207d61061a5
gonaeun <gonaeunn@gmail.com>
2024-08-26 11:51:12 +0900

    라우터 - index.js에 이어 app.js 입력

--- react06/src/App.js
+++ react06/src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Routes : 요청 URL을 묶어서 관리하는 컴포넌트
+// Route : 요청 URL에 맞는 화면 컴포넌트를 렌더링해주는 컴포넌트
+
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Routes>
+        <Route path='/' element={}/>
+        <Route path='/about' element={}/>
+        <Route path='/product' element={}/>
+        {/* 각각의 페이지의 컴포넌트 관리 >> 페이지 3개면 Route 3개 */}
+      </Routes>
     </div>
   );
 }
