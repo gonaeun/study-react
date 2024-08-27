475ffbee2f5673001ed580467126a1ecfae6330f
gonaeun <gonaeunn@gmail.com>
2024-08-27 09:45:01 +0900

    productDetailPage2 수정

--- react06/src/page/ProductDetailPage2.jsx
+++ react06/src/page/ProductDetailPage2.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 
 const ProductDetailPage2 = () => {
 
-    const {prd_no} = useParams() 
+    const [query, setQuery] = useSearchParams()
 
-    console.log(prd_no);
+  
+    console.log('전달받은 데이터:', query); // 콘솔을 통해 get이라는 객체 확인
+    console.log('전달받은 데이터:', query.get("prd_no"));  // 쿼리스트링방식은 직접 키값을 문자열 형태로 써줘야함
+    // console.log('전달받은 데이터:', query.get("prd_no"), query.get("cate"));  // 쿼리스트링 방식 이런식으로 여러개 값 적을 수 있음
     
   return (
     <div>
         <h1>ProductDetailPage2</h1>
-        <p></p>
+        <p>번 상품</p>
     </div>
   )
 }
