99237dc893a63a70d3056c7451db5d6b0b3ca34f
gonaeun <gonaeunn@gmail.com>
2024-08-27 12:21:11 +0900

    헤더 수정

--- react06_oliveyoung_practice/src/components/Header.jsx
+++ react06_oliveyoung_practice/src/components/Header.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div>
       <div>
         <Breadcrumb className='nav_divider'>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>Library</Breadcrumb.Item>
-            <Breadcrumb.Item>Data</Breadcrumb.Item>
+            <Breadcrumb.Item>
+                <Link to={"/"}>회원가입</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+                 <Link to={"/login"}>로그인</Link> 
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>
+                 <Link to={"/"}>장바구니</Link> 
+            </Breadcrumb.Item>
         </Breadcrumb>
       </div>
       <div></div>
