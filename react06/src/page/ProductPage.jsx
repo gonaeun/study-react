e9f85b7cc7997b6459b2289e80be0cf35f1b07eb
gonaeun <gonaeunn@gmail.com>
2024-08-27 09:27:35 +0900

    Update ProductPage.jsx

--- react06/src/page/ProductPage.jsx
+++ react06/src/page/ProductPage.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const ProductPage = () => {
+
+  const navigate = useNavigate();
+
+  const goToProduct = () =>{
+    // QuerySrting(쿼리스트링)방식
+    // 다음 페이지로 이동할 때 데이터를 전달하는 방식 (?key=value)
+    navigate('/productQuery?prd_no=15')
+  }
   return (
-    <div>ProductPage</div>
+    <div>
+      <h1>ProductPage</h1>
+      <button onClick={goToProduct}>ProductDetail2로 이동</button>
+    </div>
   )
 }
 
