369952439b50de1292ec02cdc4614a6bd7fb398e
gonaeun <gonaeunn@gmail.com>
2024-08-28 11:40:50 +0900

    url파라미터방식으로 보낸 주소값>> 상세페이지에서 useParams로 받아오기

--- react06_oliveyoung_practice/src/page/ProductDetails.jsx
+++ react06_oliveyoung_practice/src/page/ProductDetails.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const ProductDetails = () => {
+
+  // URL파라미터 방식으로 전달받은 값을 가져올 때, useParams() 사용
+  // *쿼리스트링 방식은 useSearchParams() 사용
+  const {id} = useParams();
+  
+  console.log('상품번호:', id);
+  
   return (
     <div>ProductDetails</div>
   )
