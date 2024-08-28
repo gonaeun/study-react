8cdffcb06f919a8ef1ebf0bc4a33dadd4bf57edb
gonaeun <gonaeunn@gmail.com>
2024-08-28 09:35:36 +0900

    데이터 담아주기

--- react06_oliveyoung_practice/src/page/MainPage.jsx
+++ react06_oliveyoung_practice/src/page/MainPage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
+import { useState } from 'react';
 
 /*
 실습1. 화면이 최초에 렌더링 되었을 때, axios를 활용해서 화장품정보를 가진 url데이터를 요청하시오
@@ -8,11 +9,16 @@ import axios from 'axios'
 
 const MainPage = () => {
 
+  const [productList, setProductList] = useState(null)
+
   // json-server로부터 화장품 정보를 가져오는 함수
   const getProductList = async () => {
     const DATA_URL = "http://localhost:3004/products"
     const response = await axios.get(DATA_URL)
     const data = response.data
+
+    setProductList(data)
+    // data 받아온 이 시점에서 setProductList함수로 data 담아주기
 
     console.log(data);
     
