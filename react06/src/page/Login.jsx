aa8489b0458379823b53565ac60bd7c1061e1700
gonaeun <gonaeunn@gmail.com>
2024-08-28 09:32:47 +0900

    데이터 요청

--- react06/src/page/Login.jsx
+++ react06/src/page/Login.jsx
@@ -6,7 +6,7 @@ const Login = ({setLoginState}) => {    // setLoginState 안에 객체 있으니
     const navigate = useNavigate();
     const goToHome = () =>{
         // 1. 로그인 상태 >> true
-        // App.js에 있는 setLoginState가 LoginState로 전달되도록 App.js의 login라우터에 변수 가져와주기
+        // App.js에 있는 LoginState로 setLoginState가 전달되도록 App.js의 login라우터에 변수 가져와주기
         // 2. '/'으로 이동
         setLoginState(true)
         navigate('/')
--- react06_oliveyoung_practice/src/page/MainPage.jsx
+++ react06_oliveyoung_practice/src/page/MainPage.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
+/*
+실습1. 화면이 최초에 렌더링 되었을 때, axios를 활용해서 화장품정보를 가진 url데이터를 요청하시오
+실습2. 받아온 데이터를 콘솔창에 출력하시오
+*/
 
 const MainPage = () => {
+
+  // json-server로부터 화장품 정보를 가져오는 함수
+  const getProductList = async () => {
+    const DATA_URL = "http://localhost:3004/products"
+    const response = await axios.get(DATA_URL)
+    const data = response.data
+
+    console.log(data);
+    
+  }
+
+  useEffect(()=>{
+    getProductList()
+  },[])
+
   return (
     <div>MainPage</div>
   )
