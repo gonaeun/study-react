6ad74ce12e2a42b5f55dee29b22306634eaa269f
gonaeun <gonaeunn@gmail.com>
2024-08-28 10:17:17 +0900

    <Col>을 12번 붙이면 원하는 화면이 나오는데... map함수로 해보자

--- react06_oliveyoung_practice/src/page/MainPage.jsx
+++ react06_oliveyoung_practice/src/page/MainPage.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect } from 'react'
 import axios from 'axios'
 import { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
 /*
 실습1. 화면이 최초에 렌더링 되었을 때, axios를 활용해서 화장품정보를 가진 url데이터를 요청하시오
 실습2. 받아온 데이터를 콘솔창에 출력하시오
+실습3. state에 저장된 화장품 정보를 map()를 활용하여 출력
+    >> Card컴포넌트 : 상품이미지, 브랜드명, 상품명, 가격, 플래그 *12번 반복
 */
 
 const MainPage = () => {
@@ -22,31 +27,35 @@ const MainPage = () => {
     // data 받아온 이 시점에서 setProductList함수로 data 담아주기
 
     console.log(data);
-    
+
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getProductList()
-  },[])
+  }, [])
 
   return (
-    <div>
-      <Card className='card'>
-        <Card.Img variant="top" src="https://image.oliveyoung.co.kr/cfimages/cf-goods/uploads/images/thumbnails/400/10/0000/0021/A00000021066105ko.jpg?l=ko" />
-        <Card.Body>
-          <Card.Title className='card_title'>
-            <p>브랜드명</p>
-            <p className='card_prd_name'>상품명</p>
-          </Card.Title>
-          <Card.Text className='card_text'>
-            28,900원
-          </Card.Text>
-          <p>
-            <span className='prd_flag' style={{backgroundColor:"red"}}>세일</span>
-          </p>
-        </Card.Body>
-      </Card>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Card className='card'>
+            <Card.Img variant="top" src="https://image.oliveyoung.co.kr/cfimages/cf-goods/uploads/images/thumbnails/400/10/0000/0021/A00000021066105ko.jpg?l=ko" />
+            <Card.Body>
+              <Card.Title className='card_title'>
+                <p>브랜드명</p>
+                <p className='card_prd_name'>상품명</p>
+              </Card.Title>
+              <Card.Text className='card_text'>
+                28,900원
+              </Card.Text>
+              <p>
+                <span className='prd_flag' style={{ backgroundColor: "red" }}>세일</span>
+              </p>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
