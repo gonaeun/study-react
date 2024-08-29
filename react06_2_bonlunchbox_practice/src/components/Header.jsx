20b575a359b5f931ec55de9f330bc44e13bf03bd
gonaeun <gonaeunn@gmail.com>
2024-08-29 10:14:06 +0900

    Update Header.jsx

--- react06_2_bonlunchbox_practice/src/components/Header.jsx
+++ react06_2_bonlunchbox_practice/src/components/Header.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header-box'>
+      <div className='header-box-logo'>
+        <img src='https://cdn.bonif.co.kr/resources/web/css/images/logo_gnb.png' alt='로고이미지'/>
+      </div>
+      <div className='header-box-nav'>
+        <ol className='header-box-nav-list'>
+          <li>메뉴소개</li>
+        </ol>
+      </div>
+      <div className='header-box-util'>
+        <button>로그인</button>
+      </div>
+    </div>
   )
 }
 
