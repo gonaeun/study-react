2acabb94d349444f0ea280ba5566ede25b3ba81d
gonaeun <gonaeunn@gmail.com>
2024-08-29 12:39:56 +0900

    제품상세페이지 클릭했을 때, 페이지에 맞는 내용 보여지도록
    (그전까진 url만 바뀌고 다 같은 내용으로 보여졌었음)

--- react06_2_bonlunchbox_practice/src/page/GoodDetail.jsx
+++ react06_2_bonlunchbox_practice/src/page/GoodDetail.jsx
@@ -1,20 +1,49 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 const GoodDetail = () => {
+
+  const {id} = useParams()
+  // id값 가져옴 >> 가져온 주소를 기반으로 상세페이지별로 정보 요청하기
+
+  const [goodDetail, setGoodDetail] = useState(null);
+
+  const getGood = async () =>{
+    const DATA_URL = `http://localhost:3004/goods_list/${id}`
+    // 가져온 id값으로 주소 가져옴 >> 주소를 기반으로 상세페이지별로 정보 요청하기
+    const res = await axios.get(DATA_URL)
+    const data = res.data
+    console.log(data);
+
+    setGoodDetail(data)
+  }
+
+  useEffect(()=>{
+    getGood()
+  },[])
+
   return (
     <div className='goods-detail-box'>
       <div className='goods-detail-box-thumb'>
-        <img src='https://cdn.bonif.co.kr/cmdt/20230704_xtR_1688446753736_93Kb.jpg' alt='도시락' />
+        <img src={goodDetail?.detail.sub_thumb} alt='도시락' />
       </div>
       <div className='goods-detail-box-info'>
         <div className='goods-detail-title'>
           <div className='goods-icon'>
+            {
+            goodDetail?.new?
             <em className='goods-new'>new</em>
+            : goodDetail?.best?
+            <em className='goods-best'>best</em>
+            : ""
+           }
             </div>
-            <div className='goods-detail-name'>탄두리&치킨 도시락</div>
-            <div className='goods-detail-txt'>한식을 넘어 세계를 담다, 세계 도시락</div>
-            <div className='goods-detail-price-box'><strong>11,800</strong>원</div>
-            <div className='goods-detail-summary'>풍미작렬 탄두리 치킨과 매콤담백 중독적인 맛의 커리. \n 고슬고슬 식감 제대로 살린 인도식 라이스까지!</div>
+            <div className='goods-detail-name'>{goodDetail?.name}</div>
+            <div className='goods-detail-txt'>{goodDetail?.detail.txt}</div>
+            <div className='goods-detail-price-box'><strong>{goodDetail?.price}</strong>원</div>
+            <div className='goods-detail-summary'>{goodDetail?.detail.summary}</div>
           </div>
         </div>
       </div>
