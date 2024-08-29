3aa892e1ea8736e71c559c1174727309cd8c7141
gonaeun <gonaeunn@gmail.com>
2024-08-29 12:01:08 +0900

    굿리스트에서 굿아이템 컴포넌트로 props 전달

--- react06_2_bonlunchbox_practice/src/components/GoodItem.jsx
+++ react06_2_bonlunchbox_practice/src/components/GoodItem.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
+// import { useParams } from 'react-router-dom'
+// import axios from 'axios'
+// import { useState, useEffect } from 'react';
 
-const GoodItem = () => {
+const GoodItem = (item) => {
+
+  console.log(item);
+  
+
   return (
     <div className='goods-item'>
       <div className='goods-item-icon'>
-        <em className='goods-new'>new</em>
+        {
+          item.new?
+          <em className='goods-new'>new</em>
+          : item.best?
+         <em className='goods-best'>best</em>
+          : ""
+        }
       </div>
 
       <div className='goods-thumb'>
-        <img src='https://cdn.bonif.co.kr/cmdt/20230704_xtR_1688446753736_93Kb.jpg' alt='도시락상품'/>
+        <img src={item.img} alt='도시락상품'/>
       </div>
       
       <div className='goods-name'>
-        <h4>찬두리 치킨&커리 반상</h4>
+        <h4>{item.name}</h4>
         <p>
-          <span>11,600</span>원
+          <span>{item.price}</span>원
         </p>
       </div>
     </div>
--- react06_2_bonlunchbox_practice/src/page/GoodList.jsx
+++ react06_2_bonlunchbox_practice/src/page/GoodList.jsx
@@ -2,23 +2,32 @@ import React from 'react'
 import axios from 'axios'
 import { useEffect } from 'react';
 import GoodItem from '../components/GoodItem'
+import { useState } from 'react';
 
 /*
 실습1. axios를 활용하여 도시락 정보를 가진 URL로 데이터 호출
 
 실습2.받아온 데이터를 보관할 STATE 생성
       (콘솔창으로 데이터 출력 >> 확인작업)
+
+실습3. state에 저장된 도시락 정보 출력
+      - 도시락 정보는 GoodItem 컴포넌트에 props로 전달
+      - map() 함수 활용
 */
 
 const GoodList = () => {
+
+  const [goods, setGoods] = useState(null)
 
 
   const getProductDetail = async () =>{
     const DATA_URL = "http://localhost:3004/goods_list"
     const res = await axios.get(DATA_URL)
     const data = res.data
-
     console.log(data);
+    
+    setGoods(data)
+    console.log('변경된State값',data);
     
   }
 
@@ -29,10 +38,9 @@ const GoodList = () => {
   
 
   return (
-    <div>
-      <GoodItem/>
-      <GoodItem/>
-      <GoodItem/>
+    <div className='goods-list'>
+      {goods?.map((item)=><GoodItem key={item.id} img={item.main_thumb} name={item.name} price={item.price}/>)}
+      
     </div>
   )
 }
