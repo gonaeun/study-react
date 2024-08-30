cdae708ea5e02e4e09d70c652752d66cee2d876e
gonaeun <gonaeunn@gmail.com>
2024-08-30 09:21:22 +0900

    로그인화면구현 (css는 classname으로 적용했음)

--- react06_2_bonlunchbox_practice/src/page/Login.jsx
+++ react06_2_bonlunchbox_practice/src/page/Login.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login-box'>
+      <div className='login-box-tit'>
+        <h2>로그인</h2>
+      </div>
+      <div className='login-box-form'>
+        <form>
+          <div className='input-block'>
+            <input type='text' placeholder='아이디를 입력해주세요'/>
+          </div>
+          <div className='input-block'>
+            <input type='password' placeholder='비밀번호를 입력해주세요'/>
+          </div>
+          <div className='submit'>
+            <input type='submit' className='login-btn' value='로그인'/>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
