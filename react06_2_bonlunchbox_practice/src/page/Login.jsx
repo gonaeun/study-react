092df6964842269f71905bad83ec7c4d03e14e13
gonaeun <gonaeunn@gmail.com>
2024-08-30 09:42:33 +0900

    로그인 페이지에서 로그인 상태판단

--- react06_2_bonlunchbox_practice/src/page/Login.jsx
+++ react06_2_bonlunchbox_practice/src/page/Login.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Login = () => {
+const Login = ({setAuth}) => {
+  // App.js에서 로그인함수 넘겨받음
+
+  const navi = useNavigate();
+
+  const handleSubmit = (e) =>{
+    e.preventDefault(); // 기본 동작을 중단하는 메소드 (이거 안하면, 로그인버튼 눌렀을 때, /login? 물음표가 뜸)
+    console.log('동작중지!');
+    
+    setAuth(true)
+    navi('/');
+    // 로그인상태로 바뀌면 메인페이지로 돌아가도록
+  }
+
   return (
     <div className='login-box'>
       <div className='login-box-tit'>
@@ -15,7 +29,7 @@ const Login = () => {
             <input type='password' placeholder='비밀번호를 입력해주세요'/>
           </div>
           <div className='submit'>
-            <input type='submit' className='login-btn' value='로그인'/>
+            <input type='submit' className='login-btn' onClick={handleSubmit} value='로그인'/>
           </div>
         </form>
       </div>
