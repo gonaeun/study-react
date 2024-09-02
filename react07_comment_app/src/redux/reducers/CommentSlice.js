2360da59646d6459d1291d5083813ba4a5d3973d
gonaeun <gonaeunn@gmail.com>
2024-09-02 09:49:05 +0900

    Update CommentSlice.js

---
+++ react07_comment_app/src/redux/reducers/CommentSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const CommentSlice = createSlice({
+    name:'',
+    initialState:{},
+    reducers:{}
+})
+
+export const CommentActions = CommentSlice.actions
+
+export default CommentSlice.reducer
