4fd79cbecae350da047644a409186ad35d56d7ec
gonaeun <gonaeunn@gmail.com>
2024-09-02 12:25:59 +0900

    props로 값 잘 받아서 화면에 보여주기

--- react07_comment_app/src/components/CommentItem.jsx
+++ react07_comment_app/src/components/CommentItem.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const CommentItem = (comment) => {
+const CommentItem = ({comment}) => {
+
+  console.log(comment);
+  
   return (
     <p>
-      <strong>오늘은 리덕스 실습날입니다!</strong> | 작성자 : 명진
+      <strong>{comment.content}</strong> | 작성자 : {comment.writer}
     </p>
   )
 }
