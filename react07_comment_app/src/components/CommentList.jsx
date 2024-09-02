fa56e65c4dcc9aaa0f4666d1e33694c7bd87f858
gonaeun <gonaeunn@gmail.com>
2024-09-02 12:47:52 +0900

    Update CommentList.jsx

--- react07_comment_app/src/components/CommentList.jsx
+++ react07_comment_app/src/components/CommentList.jsx
@@ -8,17 +8,22 @@ const CommentList = () => {
   // [store의 state 접근 방법]
   // 1. store 내에 있는 reducer의 key에 접근 : stste.comment
   // 2. slice 내에 정의된 state에 접근 : state.comment.commentList
-  const commentList = useSelector(state=>state.comment.CommentList)
+
+  // const commentList = useSelector(state=>state.comment.CommentList)
+  // >> commentSlice.js에서 CommentReducer의 {state,commentList,keyword} 중 commentList 에 접근 한 것
+  // >> 이 중, 접근해야하는 state가 여러개인 경우, 아래와 같이 구조분해할당 문법으로 저장
+  // *주의점 : 객체에 정의된 key이름과 동일한 변수명으로 정의!
+  const {CommentList, keyword} = useSelector(state=>state.comment)
   
-  console.log(commentList);
+  console.log(CommentList, keyword);
   
   
   return (
     <div>
-      <div>댓글수 : {commentList.length}</div>
+      <div>댓글수 : {CommentList.length}</div>
       {/* 배열이니까 댓글수는 length로 확인 */}
       <hr />
-      {commentList.map((item)=>(
+      {CommentList.map((item)=>(
          <CommentItem key={item.id} comment={item} />
        ))}
     </div>
