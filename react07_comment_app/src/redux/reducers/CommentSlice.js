30fe558621e822bb3824f4923053748fcf03133d
gonaeun <gonaeunn@gmail.com>
2024-09-02 11:46:29 +0900

    Update CommentSlice.js

--- react07_comment_app/src/redux/reducers/CommentSlice.js
+++ react07_comment_app/src/redux/reducers/CommentSlice.js
@@ -7,8 +7,13 @@ const CommentSlice = createSlice({
         keyword:''
     },
     reducers:{
-        addComment:()=>{
-           
+        addComment:(state, action)=>{
+            console.log(action);
+            
+            // React state 값을 직접적으로 수정 못 함 >> push가 아닌 concat 사용해야함
+            state.commentList = state.commentList.concat(action.payload) // action을 댓글내용에 추가(concat)
+            
+        
         },
         searchComment:()=>{}
     }
