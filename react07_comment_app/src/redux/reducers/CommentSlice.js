2a1b9ca4d7d0c63a67af0619f109dede85ed85e1
gonaeun <gonaeunn@gmail.com>
2024-09-02 12:19:03 +0900

    Update CommentSlice.js

--- react07_comment_app/src/redux/reducers/CommentSlice.js
+++ react07_comment_app/src/redux/reducers/CommentSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const CommentSlice = createSlice({
     name:'comment',
     initialState:{
-        commentList:[],
+        CommentList:[],
         keyword:''
     },
     reducers:{
@@ -14,7 +14,7 @@ const CommentSlice = createSlice({
             // >> push, shift, unshif 등과 같은 함수는 state를 새롭게 변경하는 동작 x
             // >> concat, map, filter(삭제), slice 등과 같은 함수는 원본을 수정하지 않고 새로운 배열 반환
             // >> push가 아닌 concat 사용해야함 (권장)
-            state.commentList = state.commentList.concat(action.payload) // action을 댓글내용에 추가(concat)
+            state.CommentList = state.CommentList.concat(action.payload) // action을 댓글내용에 추가(concat)
         },
         searchComment:()=>{}
     }
