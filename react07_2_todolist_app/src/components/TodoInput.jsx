a2bb6703117eaf85617a82f9818c008b7973b675
gonaeun <gonaeunn@gmail.com>
2024-09-03 10:51:05 +0900

    CSS 조구마한 수정

--- react07_2_todolist_app/src/components/TodoInput.jsx
+++ react07_2_todolist_app/src/components/TodoInput.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TodoInput = () => {
   return (
     <div className='todo-inputbox'>
-        <input type='text' className='todo-inputbox-input' placeholder='할 일을 입력하세요'/>
+        <input type='text' className='todo-inputbox-input' placeholder='모 할거야?'/>
         <input type='button' className='todo-inputbox-add-btn' value='등록'/>
     </div>
   )
