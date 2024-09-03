31f01ec448ad9b01b15529812ea0a18312b51cf8
gonaeun <gonaeunn@gmail.com>
2024-09-03 11:25:14 +0900

    실습 순서대로1

--- react07_2_todolist_app/src/components/TodoInput.jsx
+++ react07_2_todolist_app/src/components/TodoInput.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { v4 as uuidv4 } from 'uuid';
 
 const TodoInput = () => {
+
+    const inputRef = useRef();   // 입력한 값 가져오려면
+    const dispatch = useDispatch();  // store에 추가요청 보내려면
+    
+    const handleAddBtn = ()=>{
+        dispatch(addTodo(
+            {id:uuidv4(), text:inputRef.current.value, complete:false}
+        ))
+    }
+    
+
   return (
     <div className='todo-inputbox'>
-        <input type='text' className='todo-inputbox-input' placeholder='모 할거야?'/>
-        <input type='button' className='todo-inputbox-add-btn' value='등록'/>
+        <input type='text' className='todo-inputbox-input' placeholder='모 할거야?' ref={inputRef}/>
+        <input type='button' onClick={handleAddBtn} className='todo-inputbox-add-btn' value='등록'/>
     </div>
   )
 }
