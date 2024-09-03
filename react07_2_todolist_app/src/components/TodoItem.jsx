fe6e99b2bd4808c9cd6f0a9883b8068599ad9f06
gonaeun <gonaeunn@gmail.com>
2024-09-03 12:20:38 +0900

    아이콘도 바꾸는중

--- react07_2_todolist_app/src/components/TodoItem.jsx
+++ react07_2_todolist_app/src/components/TodoItem.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { FaCheckCircle } from "react-icons/fa";
+import { FaRegCircle } from "react-icons/fa";
 
 const TodoItem = ({todo}) => {
   return (
     <li className=''todo-item>   {/* TodoList 컴포넌트에서 ul 태그안에 TodoItem을 넣었으니까 여기선 li 여야해 */}
-        <FaCheckCircle className='todo-item-checkbox'/>
+        {/* <FaCheckCircle className='todo-item-checkbox'/> */}
+        <FaRegCircle className='todo-item-checkbox' style={{color:'lightgray'}} />
         <span className='todo-item-content'>{todo.text}</span>
         <button className='todo-item-edit-btn'>✍🏻</button>
         <button className='todo-item-delete-btn'>✂</button>
