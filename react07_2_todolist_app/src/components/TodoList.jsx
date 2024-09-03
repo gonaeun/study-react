06ad81c6d2ab373e5cdc2f344c2f72267b62088f
gonaeun <gonaeunn@gmail.com>
2024-09-03 12:15:33 +0900

    Update TodoList.jsx

--- react07_2_todolist_app/src/components/TodoList.jsx
+++ react07_2_todolist_app/src/components/TodoList.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import TodoItem from './TodoItem'
 import { useSelector } from 'react-redux'
 
-const TodoList = ({title, cheched}) => {
+const TodoList = ({title, checked}) => {
 
-    const TodoList = useSelector(state=>state.todo) //state는 store의 reducer의미하므로, store의 todo 함수 불러오기
+    const TodoList = useSelector(state=>state.todo.todoList) //state는 store의 reducer의미하므로, state.todo는 store의 todo라는 reducer이고, todoSlice의 함수를 부르려면 state.todo.todoList
 
     console.log(TodoList); // 입력한 값이 콘솔창으로 보여짐
     
@@ -15,7 +15,8 @@ const TodoList = ({title, cheched}) => {
         </p>
         <ul className='todo-llist-ul'>
             {TodoList.map((todo)=>(  // todo라는 매개변수에는 id, text, complete(true/false)값이 들어있음
-                <TodoItem/>))}
+                todo.complete === checked ? <TodoItem/> : ''
+                ))}
         
         </ul>
     </div>
