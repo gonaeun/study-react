aa07be49afebce9e9e86208f361ae5ac35fac3ac
gonaeun <gonaeunn@gmail.com>
2024-09-04 11:20:39 +0900

    콘솔로그로 영화 데이터 확인까지

--- react07_3_netflix_app/src/App.js
+++ react07_3_netflix_app/src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import api from './api'  // axios 사용할거면, 우리가 만든 api로 불러오기
 
 function App() {
+
+  const getMovieData = async() =>{
+    const API_URL = '/movie/popular?language=ko-KR&page=1';
+    // const res = await axios   // axios 사용할거면, 우리가 만든 api로 불러오기
+    const res = await api.get(API_URL)   //api에 baseURL까진 적혀있으니까 API_URL로는 그이후의 주소만 있으면 됨
+
+    console.log(res.data);
+    
+  }
+
+  useEffect(()=>{
+    getMovieData()
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
--- react07_3_netflix_app/src/api.js
+++ react07_3_netflix_app/src/api.js
@@ -1,1 +1,12 @@
 import axios from 'axios'
+
+const api = axios.create({
+    baseURL:'https://api.themoviedb.org/3',
+    headers:{
+        accept: 'application/json',  //json으로 받고싶어요
+        Authorization: `Bearer ${process.env.REACT_APP_API_ACCESS_TOKEN}`  // 토큰 위치에 내 토큰 안보이도록 .env에 적은 코드명 가져오기
+        // headers는 사이트 코드 복붙
+    }
+})
+
+export default api
