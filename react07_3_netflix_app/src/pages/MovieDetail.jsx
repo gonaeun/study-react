f39186cbad7dbbb486be11fe343fa1c2d090371b
gonaeun <gonaeunn@gmail.com>
2024-09-09 15:24:40 +0900

    Update MovieDetail.jsx
    디테일페이지 클릭했을 때, 영화포스터만 보여짐

--- react07_3_netflix_app/src/pages/MovieDetail.jsx
+++ react07_3_netflix_app/src/pages/MovieDetail.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import api from '../api' 
+import Container from 'react-bootstrap/esm/Container'
+import Col from 'react-bootstrap/esm/Col'
+import Row from 'react-bootstrap/esm/Row'
 
 const MovieDetail = () => {
+
+  const {id} = useParams()
+  const [movieInfo, setMovieInfo] = useState({})
+
+  const getMovieInfo = async () =>{
+    let res = await api.get(`/movie/${id}?language=ko-KR`)   //axios 객체를 api라는 이름으로 저장해뒀음
+
+    setMovieInfo(res.data)
+  }
+
+  useEffect(()=>{
+    getMovieInfo()
+  },[])
+
   return (
-    <div>MovieDetail</div>
+    <Container>
+      <Row>
+        <Col>
+          <img src={`https://image.tmdb.org/t/p/original${movieInfo.poster_path}`} alt='포스터 이미지'/>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
   )
 }
 
