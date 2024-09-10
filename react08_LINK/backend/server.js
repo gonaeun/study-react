1afe72f55c8bd0360245dbc7b4e36c41d4c8cd46
gonaeun <gonaeunn@gmail.com>
2024-09-10 16:16:03 +0900

    Update server.js

--- react08_LINK/backend/server.js
+++ react08_LINK/backend/server.js
@@ -4,3 +4,8 @@ const app = express()
 const indexRouter = require('./routes')
 
 app.use('/', indexRouter)
+
+app.set('port', process.env.PORT || 3001)
+app.listen(app.get('port'), ()=>{
+    console.log(`Server is running on ${app.get('port')}`);  
+})
