98b920ea0cb2b6e45098293f7b3365e9f76bb89f
gonaeun <gonaeunn@gmail.com>
2024-09-10 16:37:04 +0900

    주석추가

--- react08_LINK/backend/server.js
+++ react08_LINK/backend/server.js
@@ -5,7 +5,7 @@ const indexRouter = require('./routes')
 
 // 리액트 프로젝트 경로 설정
 const path = require('path')
-app.use(express.static(path.join(__dirname,'..','frontend','build')));  // 정적파일 연결 (npm run build 설치한 이후임)
+app.use(express.static(path.join(__dirname,'..','frontend','build')));  // 정적파일 연결 (npm run build 설치한 이후임_ npm run build란 ? 프로덕션 배포를 위한 최적화된 빌드 파일을 생성하는 기능)
 
 app.use('/', indexRouter)
 
