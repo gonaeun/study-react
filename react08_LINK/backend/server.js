1861f5b25c706f830099c36fd42f7f9b6ed70ab9
gonaeun <gonaeunn@gmail.com>
2024-09-10 16:30:45 +0900

    Update server.js

--- react08_LINK/backend/server.js
+++ react08_LINK/backend/server.js
@@ -3,6 +3,10 @@ const app = express()
 
 const indexRouter = require('./routes')
 
+// 리액트 프로젝트 경로 설정
+const path = require('path')
+app.use(express.static(path.join(__dirname,'..','frontend','build')));  // 정적파일 연결
+
 app.use('/', indexRouter)
 
 app.set('port', process.env.PORT || 3001)
