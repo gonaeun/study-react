6d912b05347079d290d84e1aa719079ec6b496a8
gonaeun <gonaeunn@gmail.com>
2024-09-10 16:50:43 +0900

    내일은 비동기통신 정의하고 라우터에서 포스트방식으로 데이터 받는거 할거야. db 연결해서 저장하는것도 할거야

--- react08_LINK/frontend/src/components/Form.jsx
+++ react08_LINK/frontend/src/components/Form.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 
 const Form = () => {
+
+    const [data,setData] = useState('')
+    
+    const handleSubmit = ()=>{
+        
+    }
+
   return (
     <div>
-        <form>
-            <input type='text'/>
+        <form onSubmit={handleSubmit}>
+            <input type='text' onChange={(e)=>setData(e.target.value)}/>
+            {/* e.target은 input태그 >> value:사용자가 입력한 값을 가져와서 >> setData 함수를 통해 state에 저장하겠다 */}
             <input type='submit' value='저장'/>
         </form>
     </div>
