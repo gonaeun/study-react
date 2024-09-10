5db4eb820092207794dd99e1a6023911335b5c70
gonaeun <gonaeunn@gmail.com>
2024-09-10 16:45:04 +0900

    화면 구성을 위한 form컴포넌트 그리고 다음단계를 위한 npm i axios 설치
    연동은 아까 끝남

---
+++ react08_LINK/frontend/src/components/Form.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <form>
+            <input type='text'/>
+            <input type='submit' value='저장'/>
+        </form>
+    </div>
+  )
+}
+
+export default Form
