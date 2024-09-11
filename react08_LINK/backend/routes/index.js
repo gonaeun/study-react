60a0c4b61bd613f3d4f4b957126bd8d61f45b29e
gonaeun <gonaeunn@gmail.com>
2024-09-11 14:21:39 +0900

    Update index.js
    방금은 비동기통신 하려고 버튼눌렀을때 데이터 전송 기능 막아지는 기능 했고, 지금은 똑같이 /getData라는 경로 사용해서 Form페이지로 요청 받음!

--- react08_LINK/backend/routes/index.js
+++ react08_LINK/backend/routes/index.js
@@ -7,4 +7,9 @@ router.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'..','..','frontend','build','index.html'));
 })
 
+router.post('/getData', (req,res)=>{
+    console.log('Form으로 요청이 들어옴');
+    
+})
+
 module.exports = router
