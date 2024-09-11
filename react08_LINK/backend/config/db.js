ab067dd81fcc9bc8a02d8bd6df75f1a1a7090bfa
gonaeun <gonaeunn@gmail.com>
2024-09-11 14:43:58 +0900

    Update db.js

--- react08_LINK/backend/config/db.js
+++ react08_LINK/backend/config/db.js
@@ -1,5 +1,13 @@
 const mysql = require('mysql2')
 
 const conn = mysql.createConnection({
-    
+    host : 'localhost',
+    user:'root',
+    password:'1234',
+    port:3306
+    database:''
 })
+
+conn.connect();
+
+module.exports = conn;
