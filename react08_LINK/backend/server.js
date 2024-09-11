2c5b57978bb8bfcba3bf7b4d3f1d102e1096832b
gonaeun <gonaeunn@gmail.com>
2024-09-11 17:27:18 +0900

    Update server.js

--- react08_LINK/backend/server.js
+++ react08_LINK/backend/server.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const app = express()
 
-const indexRouter = require('./routes')  // 라우터 설정
+const indexRouter = require('./routes')  // 라우터 모듈 가져오기
 
 // 쿼리스트링 및 JSON 형식의 데이터 파싱
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-// 라우터 설정
-app.use('/', indexRouter)
 
 // 리액트 프로젝트 경로 설정
 const path = require('path')
 app.use(express.static(path.join(__dirname,'..','frontend','build')));  // 정적파일 연결 (npm run build 설치한 이후)
+
+// 루트 경로에 라우터 연결
+app.use('/', indexRouter)
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '..', 'frontend', 'build', 'index.html'));
