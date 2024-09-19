896febcd0cf1af5c8ab4c2413cfade5a8d05fc38
gonaeun <gonaeunn@gmail.com>
2024-09-19 15:30:00 +0900

    회원가입창에서 res.data 봐야지

--- react08_2_FINAL_PROJECT/frontend/src/pages/Join.jsx
+++ react08_2_FINAL_PROJECT/frontend/src/pages/Join.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import { useRef } from 'react'
+import React, { useRef } from 'react'
 import api from '../api' 
+import { useNavigate } from 'react-router-dom'
 
 const Join = () => {
 
     const id_ref = useRef()
     const pw_ref = useRef()
     const nick_ref = useRef()
+    const navigate = useNavigate()
 
     const sendJoin = async (e) =>{
         e.preventDefault()
@@ -17,21 +18,28 @@ const Join = () => {
             nick : nick_ref.current.value
         }
         let res = await api.post('/user/join', joinData);
+        console.log(res.data);
+
+        if(res.data.result === "success"){
+            navigate("/");
+        }else{
+            alert("회원가입 오류발생")
+        }
     }
 
   return (
     <div className='content'>
         <form onSubmit={sendJoin}>
             <p>
-                <label>ID:</label>
+                <label>ID: </label>
                 <input type='text' ref={id_ref}/>
             </p>
             <p>
-                <label>PW:</label>
+                <label>PW: </label>
                 <input type='password' ref={pw_ref}/>
             </p>
             <p>
-                <label>NICK:</label>
+                <label>NICK: </label>
                 <input type='text' ref={nick_ref}/>
             </p>
             <p>
