efa7855b958abd8689b96fc9d16fd9f50466633c
gonaeun <gonaeunn@gmail.com>
2024-09-19 14:42:18 +0900

    npm i cors

--- react08_2_FINAL_PROJECT/backend/server.js
+++ react08_2_FINAL_PROJECT/backend/server.js
@@ -2,14 +2,21 @@ const express = require('express')
 const app = express()
 const indexRouter = require('./routes')
 const userRouter = require('./routes/user')
-// const path = require('path')
+
+// 교차 출처 리소스 공유(CORS)
+// - 같은 도메인이 아닌 경우에 요청/응답 제한
+// - 모든 요청/응답에 대해 허용하거나, 특정 도메인에 대해 허용 해줄 경우 >> npm i cors 설치
+const cors = require('cors')
+
+app.use(cors())
+app.use('/', indexRouter)
+app.use('/user', userRouter)
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 
-app.use('/',indexRouter)
-app.use('/user',userRouter)
 
+// const path = require('path')
 // app.use(express.static(path.join(__dirname,'..','frontend','build')))
 
 app.set('port',process.env.PORT || 3000)
--- react08_2_FINAL_PROJECT/frontend/src/pages/Join.jsx
+++ react08_2_FINAL_PROJECT/frontend/src/pages/Join.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import { useRef } from 'react'
+import api from '../api' 
 
 const Join = () => {
 
     const id_ref = useRef()
     const pw_ref = useRef()
     const nick_ref = useRef()
+
+    const sendJoin = async (e) =>{
+        e.preventDefault()
+        
+        let joinData = {
+            id : id_ref.current.value,
+            pw : pw_ref.current.value,
+            nick : nick_ref.current.value
+        }
+        let res = await api.post('/user/join', joinData)
+    }
 
   return (
     <div className='content'>
