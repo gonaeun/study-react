03c8834754f528cedff5204a80602a4007b0e9a0
gonaeun <gonaeunn@gmail.com>
2024-09-19 14:32:19 +0900

    Update Join.jsx
    아까 fronted에서 선생님이 api.js도 입력했음

--- react08_2_FINAL_PROJECT/frontend/src/pages/Join.jsx
+++ react08_2_FINAL_PROJECT/frontend/src/pages/Join.jsx
@@ -1,23 +1,26 @@
 import React from 'react'
+import { useRef } from 'react'
 
 const Join = () => {
 
-
+    const id_ref = useRef()
+    const pw_ref = useRef()
+    const nick_ref = useRef()
 
   return (
     <div className='content'>
         <form onSubmit={sendJoin}>
             <p>
                 <label>ID:</label>
-                <input type='text'/>
+                <input type='text' ref={id_ref}/>
             </p>
             <p>
                 <label>PW:</label>
-                <input type='password'/>
+                <input type='password' ref={pw_ref}/>
             </p>
             <p>
                 <label>NICK:</label>
-                <input type='text'/>
+                <input type='text' ref={nick_ref}/>
             </p>
             <p>
                 <input type='submit' value='Login'/>
