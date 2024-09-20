0f699f1dfd6a92b3fbf8ad8a3632bb71b5dc8585
gonaeun <gonaeunn@gmail.com>
2024-09-20 16:53:47 +0900

    마커 한꺼번에 안뜨고 하나씩만 뜨도록 카카오사이트에 있는 코드 보고 수정함

--- react08_3_node_map/frontend/src/components/MapComponent.jsx
+++ react08_3_node_map/frontend/src/components/MapComponent.jsx
@@ -1,75 +1,78 @@
-import React, { useEffect, useState } from 'react'
-import { Map, MapMarker } from "react-kakao-maps-sdk"
-import useKakaoLoader from './useKakaoLoader'
-import api from '../api'
+import React, { useEffect, useState } from 'react';
+import { Map, MapMarker, useMap } from "react-kakao-maps-sdk";
+import useKakaoLoader from './useKakaoLoader';
+import api from '../api';
 
 const MapComponent = () => {
     useKakaoLoader();
 
-    // 인포윈도우 Open 여부를 저장하는 state 입니다.
-    const [isOpen, setIsOpen] = useState(false)
-    const [position, setPosition] = useState(null)
-
+    const [position, setPosition] = useState(null);
 
     // 현재 위치(위경도) 확인
     const getCurrentPosition = () => {
         navigator.geolocation.getCurrentPosition((position) => {
             console.log("현재 위치:", position);
         });
-        // F12 - 콘솔창에서 객체로 확인 가능
     };
 
     // 서버로부터 위치정보 요청
     const getGwangjuPlace = async () => {
-        let res = await api.get('/place/position')
-
+        let res = await api.get('/place/position');
         console.log(res.data);
-
-        setPosition(res.data.position)
-    }
+        setPosition(res.data.position);
+    };
 
     useEffect(() => {
         getCurrentPosition();
-        getGwangjuPlace()
+        getGwangjuPlace();
     }, []);
 
     return (
         <div>
-            <Map // 지도를 표시할 Containersr
+            <Map
                 id="map"
                 center={{
-                    // 지도의 중심좌표
-                    // 인공지능사관학교
                     lat: 35.14656855941232,
                     lng: 126.92220584707904,
-                    // 카카오 제주도 본사
-                    // lat: 33.450701,
-                    // lng: 126.570667,
                 }}
                 style={{
-                    // 지도의 크기
                     width: "100%",
                     height: "350px",
                 }}
-                level={3} // 지도의 확대 레벨
+                level={3}
             >
-               {position?.map((pos) => (
-                    <MapMarker
+                {position?.map((pos) => (
+                    <EventMarkerContainer
+                        key={`EventMarkerContainer-${pos.latlng.lat}-${pos.latlng.lng}`}
                         position={pos.latlng}
-                        clickable={true}
-                        onMouseOver={() => setIsOpen(true)}
-                        onMouseOut={() => setIsOpen(false)}
-                    >
-                        {isOpen && (
-                            <div style={{ padding: "5px", color: "#000" }}>
-                                {pos.title}
-                            </div>
-                        )}
-                    </MapMarker>
+                        title={pos.title}
+                    />
                 ))}
             </Map>
         </div>
-    )
-}
+    );
+};
 
-export default MapComponent
+// 개별 마커 상태를 관리하는 컴포넌트
+const EventMarkerContainer = ({ position, title }) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const map = useMap();
+
+    return (
+        <MapMarker
+            position={position}
+            clickable={true}
+            onClick={(marker) => map.panTo(marker.getPosition())}
+            onMouseOver={() => setIsVisible(true)}
+            onMouseOut={() => setIsVisible(false)}
+        >
+            {isVisible && (
+                <div style={{ padding: "5px", color: "#000" }}>
+                    {title}
+                </div>
+            )}
+        </MapMarker>
+    );
+};
+
+export default MapComponent;
